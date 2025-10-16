33a4dc82fb8914a019c9077a48c3b9ac8fbd4492
barpurzycki <92019696+barpurzycki@users.noreply.github.com>
2025-10-16 20:59:33 +0200

    Update lab1.go

--- lab1.go
+++ lab1.go
@@ -3,11 +3,28 @@ package main
 import (
 	"fmt"
 	"math"
+	"time"
 )
 
+const n int = 1024
+
+var threads int = 16
+var tab [n]float64
+var sum2 float64 = 0
+
+func suma(id int, segment int) {
+	start := id * segment
+	end := start + segment
+	localSum := 0.0
+	for i := start; i < end; i++ {
+		localSum += tab[i]
+	}
+	sum2 += localSum
+}
+
 func main() {
-	const n int = 1024
-	var tab [n]float64
+	// Zadanie 1 i 2
+
 	for i := 0; i < n; i++ {
 		angle := float64(i) * 2 * math.Pi / float64(n)
 		tab[i] = math.Sin(angle)
@@ -18,5 +35,17 @@ func main() {
 		sum += tab[i]
 	}
 
-	fmt.Printf("%.10f", sum)
+	fmt.Printf("Suma 1 wątku: %.20f\n", sum)
+
+	// Zadanie 3
+
+	segment := n / threads
+	for j := 0; j < threads; j++ {
+		go suma(j, segment)
+	}
+
+	time.Sleep(1 * time.Second)
+
+	fmt.Printf("Suma 16 wątków: %.20f", sum2)
+
 }
