605064ea3946452d6c2cd0ee64e3ddc62ece26f1
barpurzycki <92019696+barpurzycki@users.noreply.github.com>
2025-10-17 11:51:51 +0200

    Create lab2.go

---
+++ lab2.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	//"time"
+)
+
+var mut sync.RWMutex
+var wg sync.WaitGroup
+
+const n int = 10000
+
+var tab [n]float64
+
+func statystyka(tab [n]float64) float64 {
+	sum := 0.0
+	for _, val := range tab {
+		sum += val
+	}
+	return sum
+}
+
+func czytacz() {
+	defer wg.Done()
+	mut.RLock()
+	fmt.Println(statystyka(tab))
+	mut.RUnlock()
+}
+
+func main() {
+
+	for i := 0; i < n/2; i++ {
+		tab[i] = 1
+	}
+
+	for i := n / 2; i < n; i++ {
+		tab[i] = -1
+	}
+
+	fmt.Println("Suma wartości tablicy: ", statystyka(tab))
+	go czytacz()
+	go czytacz()
+	go czytacz()
+}
