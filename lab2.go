760ea7faf849035ec49f364ccd8570447d81489e
barpurzycki <92019696+barpurzycki@users.noreply.github.com>
2025-10-23 17:59:44 +0200

    Update lab2.go

--- lab2.go
+++ lab2.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"sync"
-	//"time"
 )
 
 var mut sync.RWMutex
@@ -13,19 +12,33 @@ const n int = 10000
 
 var tab [n]float64
 
-func statystyka(tab [n]float64) float64 {
+func statystyka(tab *[n]float64) (float64, float64) {
 	sum := 0.0
+	max := tab[0]
 	for _, val := range tab {
 		sum += val
+		if val > max {
+			max = val
+		}
 	}
-	return sum
+	return sum, max
 }
 
 func czytacz() {
 	defer wg.Done()
 	mut.RLock()
-	fmt.Println(statystyka(tab))
+	sum, max := statystyka(&tab)
+	fmt.Printf("Suma: %.2f, Max: %.2f\n", sum, max)
 	mut.RUnlock()
+}
+
+func pisarzMnoznik() {
+	defer wg.Done()
+	mut.Lock()
+	for i := range tab {
+		tab[i] *= 2
+	}
+	mut.Unlock()
 }
 
 func main() {
@@ -38,10 +51,39 @@ func main() {
 		tab[i] = -1
 	}
 
-	fmt.Println("Suma wartości tablicy: ", statystyka(tab))
+	fmt.Printf("Zadanie 2:\n")
+	sum, max := statystyka(&tab)
+	fmt.Printf("Suma wartości tablicy: %.2f, Maksymalna wartość: %.2f\n\n", sum, max)
+
+	fmt.Printf("Zadanie 3:\n")
 	wg.Add(3)
 	go czytacz()
 	go czytacz()
 	go czytacz()
 	wg.Wait()
+
+	sum, max = statystyka(&tab)
+	fmt.Printf("Suma wartości tablicy: %.2f, Maksymalna wartość: %.2f\n\n", sum, max)
+
+	fmt.Printf("Zadanie 4:\n")
+	wg.Add(3)
+	go pisarzMnoznik()
+	go pisarzMnoznik()
+	go pisarzMnoznik()
+	wg.Wait()
+
+	sum, max = statystyka(&tab)
+	fmt.Printf("Suma wartości tablicy: %.2f, Maksymalna wartość: %.2f\n\n", sum, max)
+
+	fmt.Printf("Zadanie 5:\n")
+	wg.Add(5)
+	go czytacz()
+	go czytacz()
+	go czytacz()
+	go pisarzMnoznik()
+	go pisarzMnoznik()
+	wg.Wait()
+
+	sum, max = statystyka(&tab)
+	fmt.Printf("Suma wartości tablicy: %.2f, Maksymalna wartość: %.2f\n", sum, max)
 }
