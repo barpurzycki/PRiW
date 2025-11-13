5069d347c02b559fab62c137a653aa8bf7ff34b1
barpurzycki <92019696+barpurzycki@users.noreply.github.com>
2025-11-13 22:17:47 +0100

    Update lab4.go

--- lab4.go
+++ lab4.go
@@ -5,7 +5,11 @@ import (
 	"sync"
 )
 
+var wg sync.WaitGroup
+
 func main() {
+	//Zadanie 1
+
 	const size = 300
 	tab := make([]float64, size)
 
@@ -29,14 +33,128 @@ func main() {
 	fmt.Println(tab_cz2[0])
 	fmt.Println(tab_cz3[0])
 
-	var kanal chan int
-	kanal := make(chan int, 3)
+	//Zadanie 2
 
-	var wg sync.WaitGroup
+	kanal2 := make(chan float64, 3)
+
+	sumuj := func(dane []float64) {
+		defer wg.Done()
+		var suma float64
+		for _, v := range dane {
+			suma += v
+		}
+		kanal2 <- suma
+	}
+
+	wg.Add(3)
+	go sumuj(tab_cz1)
+	go sumuj(tab_cz2)
+	go sumuj(tab_cz3)
+
+	go func() {
+		wg.Wait()
+		close(kanal2)
+	}()
+
+	for s := range kanal2 {
+		fmt.Printf("Suma czesci: %.2f\n", s)
+	}
+	fmt.Println()
+
+	//Zadanie 3
+
+	kanal3 := make(chan float64, 3)
+
+	sumuj3 := func(dane []float64) {
+		defer wg.Done()
+		var suma float64
+		for _, v := range dane {
+			suma += v
+		}
+		kanal3 <- suma
+	}
+
+	pusty := func() {
+		defer wg.Done()
+	}
+
+	wg.Add(8)
+	go sumuj3(tab_cz1)
+	go sumuj3(tab_cz2)
+	go sumuj3(tab_cz3)
+	for i := 0; i < 5; i++ {
+		go pusty()
+	}
+
+	go func() {
+		wg.Wait()
+		close(kanal3)
+	}()
+
+	for s := range kanal3 {
+		fmt.Printf("Suma: %.2f\n", s)
+	}
+	fmt.Println()
+
+	//Zadanie 4
+
+	kanal4 := make(chan float64, 3)
+
+	sumuj4 := func(dane []float64) {
+		defer wg.Done()
+		var suma float64
+		for _, v := range dane {
+			suma += v
+		}
+		kanal4 <- suma
+	}
 
 	l_watkow := 2
+	wg.Add(l_watkow)
 
-	for i := 0; i < l_watkow; i++ {
-		wg.Add(1)
+	go sumuj4(tab_cz1)
+	go sumuj4(tab_cz2)
+
+	go func() {
+		wg.Wait()
+		close(kanal4)
+	}()
+
+	for s := range kanal4 {
+		fmt.Printf("Suma: %.2f\n", s)
 	}
+	fmt.Println()
+
+	//Zadanie 5
+
+	kanal5 := make(chan float64, 3)
+
+	sumuj5 := func(dane []float64) {
+		defer wg.Done()
+		var suma float64
+		for _, v := range dane {
+			suma += v
+		}
+		kanal5 <- suma
+	}
+
+	wg.Add(3)
+	go sumuj5(tab_cz1)
+	go sumuj5(tab_cz2)
+	go sumuj5(tab_cz3)
+
+	go func() {
+		wg.Wait()
+		close(kanal5)
+	}()
+
+	var sumaCalkowita float64
+	i := 1
+	for wartosc := range kanal5 {
+		fmt.Printf("Suma czesci %d: %.2f\n", i, wartosc)
+		sumaCalkowita += wartosc
+		i++
+	}
+
+	fmt.Printf("Suma calkowita: %.2f\n", sumaCalkowita)
 }
